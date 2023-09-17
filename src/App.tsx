00cf52bcfb80b23c395934ad93eb79cb37d9c389
Noor Eid <noorhamdyeid19@gmail.com>
2023-09-17 13:07:27 +0300

    removed react query dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthProvider } from "./components/AuthProvider";
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
         <AuthProvider>
           <RouterProvider router={router}></RouterProvider>
         </AuthProvider>
-        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
       </QueryClientProvider>
     </>
   );
